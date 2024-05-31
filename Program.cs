82614713cdb0a7b6d2cbe62cad85430b9babc64e
test <test@MANSAI>
2024-05-31 14:19:02 +1000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿//CAB301 - Assignment 3B - Main program
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace assignment3B
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu menu = new Menu();
+            TransportationNetwork graph_expected = new TransportationNetwork();
+            while (true)
+            {
+                menu.DisplayMenu();
+                int choice = menu.GetUserChoice();
+                
+                menu.PerformUserAction(choice, ref graph_expected);
+            }
+            
+        }
+    }
+}
+
